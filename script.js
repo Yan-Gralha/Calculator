d7002e3b37f72656a727b13bfd134b70439c5a94
Yan Ambrosio Gralha <yan.ambrosio.gralha@gmail.com>
2025-12-03 15:45:29 -0300

    Removendo comentáros

--- script.js
+++ script.js
@@ -1,86 +1,84 @@
-
 function insertNumber(num) {
-    if (result.innerHTML.length < 13) { //Limite de 13 caracteres
-        result.innerHTML += num; //Adiciona os números clicados a variável result, mostrando eles na tela
+    if (result.innerHTML.length < 13) {
+        result.innerHTML += num;
     }
-};
+}
 function clearDisplay() {
-    result.innerHTML = ""; //Reseta os valores da variável result
-};
+    result.innerHTML = "";
+}
 function backspace() {
-    result.innerHTML = result.innerHTML.slice(0, -1); //Corta o último caractere inserido
-};
+    result.innerHTML = result.innerHTML.slice(0, -1);
+}
 function sum() {
-    if (result.innerHTML.length < 13) { // Limite de 13 caracteres
-        let lastChar = result.innerHTML.slice(-1); // Pega o último caractere inseridos
-        if (!"+-×÷.".includes(lastChar)) { //Verifica se o último caractere inserido não é +-×÷.
-            result.innerHTML += "+"; //Se não for nenhum deles, adiciona um + na variável result
+    if (result.innerHTML.length < 13) {
+        let lastChar = result.innerHTML.slice(-1);
+        if (!"+-×÷.".includes(lastChar)) {
+            result.innerHTML += "+";
         }
     }
-};
+}
 function min() {
-    if (result.innerHTML.length < 13) { // Limite de 13 caracteres
-        let lastChar = result.innerHTML.slice(-1); // Pega o último caractere inserido
-        if (!"+-×÷.".includes(lastChar)) { //Verifica se o último caractere inserido não é +-×÷.
-            result.innerHTML += "-"; //Se não for nenhum deles, adiciona um - na variável result
+    if (result.innerHTML.length < 13) {
+        let lastChar = result.innerHTML.slice(-1);
+        if (!"+-×÷.".includes(lastChar)) {
+            result.innerHTML += "-";
         }
     }
-};
+}
 function mult() {
-    if (result.innerHTML.length < 13) { // Limite de 13 caracteres
-        let lastChar = result.innerHTML.slice(-1); // Pega o último caractere inserido
-        if (!"+-×÷.".includes(lastChar)) { //Verifica se o último caractere inserido não é +-×÷.
-            result.innerHTML += "×"; //Se não for nenhum deles, adiciona um × na variável result
+    if (result.innerHTML.length < 13) {
+        let lastChar = result.innerHTML.slice(-1);
+        if (!"+-×÷.".includes(lastChar)) {
+            result.innerHTML += "×";
         }
     }
-};
+}
 function div() {
-    if (result.innerHTML.length < 13) { // Limite de 13 caracteres
-        let lastChar = result.innerHTML.slice(-1); // Pega o último caractere inserido
-        if (!"+-×÷.".includes(lastChar)) { //Verifica se o último caractere inserido não é +-×÷.
-            result.innerHTML += "÷"; //Se não for nenhum deles, adiciona um ÷ na variável result
+    if (result.innerHTML.length < 13) {
+        let lastChar = result.innerHTML.slice(-1);
+        if (!"+-×÷.".includes(lastChar)) {
+            result.innerHTML += "÷";
         }
     }
-};
+}
 function dot() {
-    if (result.innerHTML.length < 13) { // Limite de 13 caracteres
-        let lastChar = result.innerHTML.slice(-1); // Pega o último caractere inserido
-        if (!"+-×÷.".includes(lastChar)) { //Verifica se o último caractere inserido não é +-×÷.
-            result.innerHTML += "."; //Se não for nenhum deles, adiciona um . na variável result
+    if (result.innerHTML.length < 13) {
+        let lastChar = result.innerHTML.slice(-1);
+        if (!"+-×÷.".includes(lastChar)) {
+            result.innerHTML += ".";
         }
     }
-};
-document.addEventListener("keydown", function (event) { //Ações com o teclado
-    if (event.key >= "0" && event.key <= "9") { //Diz que se apertar as teclas de 0 a 9 vai executar a função insertNumber() com o valor pressionado
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key >= "0" && event.key <= "9") {
         insertNumber(event.key);
-    } else if (event.key === "Backspace") { //Diz que se apertar Backspace vai executar a função backspace()
+    } else if (event.key === "Backspace") {
         backspace();
-    } else if (event.key === "Delete") {//Diz que se apertar Delete vai executar a função clearDisplay()
+    } else if (event.key === "Delete") {
         clearDisplay();
-    } else if (event.key === "+") {//Diz que se apertar + vai executar a função sum()
+    } else if (event.key === "+") {
         sum();
-    } else if (event.key === "-") {//Diz que se apertar - vai executar a função min()
+    } else if (event.key === "-") {
         min();
-    } else if (event.key === "*") {//Diz que se apertar * vai executar a função mult()
+    } else if (event.key === "*") {
         mult();
-    } else if (event.key === "/") {//Diz que se apertar / vai executar a função div()
+    } else if (event.key === "/") {
         div();
-    } else if (event.key === ".") {//Diz que se apertar . vai executar a função dot()
+    } else if (event.key === ".") {
         dot();
-    } else if (event.key === "Enter") {//Diz que se apertar Enter vai executar a função calculate()
+    } else if (event.key === "Enter") {
         calculate();
     }
 });
-function calculate() {
-    let expression = result.innerHTML; //Fala que a variável expression tem o valor do que estiver na variável result
 
-    // Substituir operadores visuais pelos operadores do JavaScript
+function calculate() {
+    let expression = result.innerHTML;
     expression = expression.replace(/×/g, '*').replace(/÷/g, '/');
 
     try {
-        // Avaliar a expressão e exibir o resultado
-        result.innerHTML = eval(expression); //A função eval() avalia a string result como código JavaScript e executa essa string como se fosse um comando do próprio código
+        result.innerHTML = eval(expression);
     } catch (error) {
-        result.innerHTML = "Erro"; //Se der erro exibe "Erro"
+        result.innerHTML = "Erro";
     }
-};
+}
